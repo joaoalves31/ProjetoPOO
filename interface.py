0a04d6ddc0f84469af5785a2af72d0f26b091ab3
joaoalves31 <100239569+joaoalves31@users.noreply.github.com>
2025-01-23 08:13:04 -0300

    Volta o botão de criar conta

--- interface.py
+++ interface.py
@@ -250,8 +250,6 @@ class BancoApp:
             value="ContaPoupança"
         ).pack(pady=5)
 
-        ctk.CTkButton(frame, text="Sair", command=self.encerrar_sessao).pack(pady=10)
-
         # Botão Criar Conta
         ctk.CTkButton(
             frame, 
